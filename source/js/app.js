aabad60374fc67a286c288fcff2170927933172b
malienkoN <nekmalienko2001@gmail.com>
2021-04-25 13:00:50 +0300

    header mobile adaptive start

--- source/js/app.js
+++ source/js/app.js
@@ -5,4 +5,11 @@ $(document).ready(function () {
         $('.tel-dropdown').fadeToggle();
         $('.header__action-icon__dropdown').toggleClass('rotate');
     })
+
+    $('.burger-mobile').click(function (event) {
+        event.preventDefault();
+
+        $('.header__bottom').fadeToggle();
+        $(this).toggleClass('active');
+    })
 });
