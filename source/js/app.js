abb9f1f93c695a0b195bffc0e84073106c5d95a5
malienkoN <nekmalienko2001@gmail.com>
2021-05-01 20:25:56 +0300

    card

--- source/js/app.js
+++ source/js/app.js
@@ -13,7 +13,37 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     })
 
-    const swiper = new Swiper('.swiper-container', {
+    new Swiper('.intro-swiper', {
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    //tabs
+    $('.tabs__triggers-item').click(function (e) {
+        e.preventDefault();
+
+        $('.tabs__triggers-item').removeClass('tabs__triggers-item--active');
+        $('.tabs__content-item').removeClass('tabs__content-item--active');
+
+        $(this).addClass('tabs__triggers-item--active');
+        $($(this).attr('href')).addClass('tabs__content-item--active');
+    });
+
+    $('.tabs__triggers-item:first').click();
+
+    new Swiper('.tabs-swiper', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 30,
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
