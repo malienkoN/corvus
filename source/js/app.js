40e986179e9fa9bcd101021d4f737d17cc0af420
malienkoN <nekmalienko2001@gmail.com>
2021-05-25 21:58:03 +0300

    contacts.html

--- source/js/app.js
+++ source/js/app.js
@@ -273,3 +273,38 @@ $(document).ready(function () {
     mainPinMarkerAbout.addTo(mapAbout);
     mainPinMarkerAbout.bindPopup("проспект Свободи,<br> 75, Кам'янське");
 });
+
+{
+    //map 4
+    const mapContacts = L.map('mapContacts')
+        .setView({
+            lat: 48.50446,
+            lng: 34.61024,
+        }, 15);
+
+    L.tileLayer(
+        'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+        {
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        },
+    ).addTo(mapContacts);
+
+    const mainPinIconContacts = L.icon({
+        iconUrl: 'img/map-pin.png',
+        iconSize: [30, 47],
+        iconAnchor: [15, 47],
+    });
+
+    const mainPinMarkerContacts = L.marker(
+        {
+            lat: 48.50446,
+            lng: 34.61024,
+        },
+        {
+            icon: mainPinIconContacts,
+        },
+    );
+
+    mainPinMarkerContacts.addTo(mapContacts);
+    mainPinMarkerContacts.bindPopup("проспект Свободи,<br> 75, Кам'янське");
+}
